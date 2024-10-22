80480fff94258168004d6c91455f251f72bdeefd
pau ros moleon <116161072+paurosmoleon@users.noreply.github.com>
2024-10-22 22:27:25 +0200

    ajustando el tic tac toe

--- front/js/game.js
+++ front/js/game.js
@@ -1,7 +1,40 @@
 window.onload = function() {
-    const tablero = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-    ]
+
+    
+
+        const tablero = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8]
+        ]
+        let index = 0
+        let player = 1
+        const cells = document.querySelectorAll('.cell')
+        const img_ttt = document.querySelectorAll('.cell_img')
+        tablero.forEach( row =>{
+            row.forEach(column => {
+                const cell = cells[index]
+                const img = img_ttt[index]
+               cell.addEventListener('click',() => {
+                    if(img_ttt){
+                         console.log(img.src)
+                        if (img.src === '')
+                        {
+
+                            if (player === 1) {
+                               img.src = '../assets/img/game/2029570.svg'
+                               player = 2
+                            } else { 
+                              img.src = '../assets/img/game/947530-ffffff.svg'
+                              player = 1
+                            }
+                        }
+                    }
+               })
+               index++
+            })
+        } )
+
+
+    
 }
