e8ff663f06b3c7bc4659e4e2daec6562454100c5
Reo <fergonesteban03@gmail.com>
2024-10-23 19:41:52 +0200

    ico,adivinanzas y js
    +ico de pag
    +adivinanzas e imagenes
    +js de las adivinanzas

---
+++ front/js/index.js
@@ -0,0 +1,30 @@
+function verificarNombreJose() {
+    const nombre = document.getElementById("checkJose").value;
+    const imagenDiv = document.getElementById("imagenJose");
+
+    if (nombre === "Jose") {
+      imagenDiv.style.display = "block";
+    } else {
+      imagenDiv.style.display = "none"; 
+    }
+  }
+  function verificarNombreJavi() {
+    const nombre = document.getElementById("checkJavi").value;
+    const imagenDiv = document.getElementById("imagenJavi");
+
+    if (nombre === "Javi") {
+      imagenDiv.style.display = "block";
+    } else {
+      imagenDiv.style.display = "none"; 
+    }
+  }
+  function verificarNombreJuli() {
+    const nombre = document.getElementById("checkJuli").value;
+    const imagenDiv = document.getElementById("imagenJuli");
+
+    if (nombre === "Juli") {
+      imagenDiv.style.display = "block";
+    } else {
+      imagenDiv.style.display = "none"; 
+    }
+  }
