01a572c4c131e2c7e9f179c2ebc3f16d38bd7427
Reo <fergonesteban03@gmail.com>
2024-10-23 20:43:09 +0200

    js terminados
    js de las adivinanzas terminados

--- front/js/index.js
+++ front/js/index.js
@@ -1,30 +1,52 @@
 function verificarNombreJose() {
-    const nombre = document.getElementById("checkJose").value;
-    const imagenDiv = document.getElementById("imagenJose");
+  const nombre = document.getElementById("checkJose").value;
+  const imagenDiv = document.getElementById("imagenJose");
 
-    if (nombre === "Jose") {
-      imagenDiv.style.display = "block";
-    } else {
-      imagenDiv.style.display = "none"; 
-    }
-  }
-  function verificarNombreJavi() {
-    const nombre = document.getElementById("checkJavi").value;
-    const imagenDiv = document.getElementById("imagenJavi");
+  if (nombre === "Jose") {
+    imagenDiv.style.display = "block";
+  } 
+}
+function verificarNombreJavi() {
+  const nombre = document.getElementById("checkJavi").value;
+  const imagenDiv = document.getElementById("imagenJavi");
 
-    if (nombre === "Javi") {
-      imagenDiv.style.display = "block";
-    } else {
-      imagenDiv.style.display = "none"; 
-    }
-  }
-  function verificarNombreJuli() {
-    const nombre = document.getElementById("checkJuli").value;
-    const imagenDiv = document.getElementById("imagenJuli");
+  if (nombre === "Javi") {
+    imagenDiv.style.display = "block";
+  } 
+}
+function verificarNombreJuli() {
+  const nombre = document.getElementById("checkJuli").value;
+  const imagenDiv = document.getElementById("imagenJuli");
 
-    if (nombre === "Juli") {
-      imagenDiv.style.display = "block";
-    } else {
-      imagenDiv.style.display = "none"; 
+  if (nombre === "Juli") {
+    imagenDiv.style.display = "block";
+    var sonido = document.getElementById("sonidoBloodborne");
+    sonido.currentTime = 0; 
+    sonido.play(); 
+  } 
+}
+ function navidadEnHalloween() {
+   document.body.style.backgroundImage ="url('./assets/img/fondo_navidad.webp')";
+   document.body.style.backgroundSize ="cover";
+   document.body.style.backgroundPosition ="center";
+   document.body.style.backgroundRepeat ="no-repeat";
+   document.body.style.backgroundAttachment ="fixed";
+
+    setTimeout(() => {
+        document.body.style.backgroundImage = "url('./assets/img/fondo.webp')"; 
+        document.body.style.backgroundSize ="cover";
+        document.body.style.backgroundPosition ="center";
+        document.body.style.backgroundRepeat ="no-repeat";
+        document.body.style.backgroundAttachment ="fixed";
+    }, 3000); 
+
+ }
+  function sonidoCaballo() {
+        var sonido = document.getElementById("sonidoCaballo");
+        sonido.currentTime = 0; 
+        sonido.play(); 
     }
-  }
+  
+  function sonidoBloodborne() {
+
+    }
