2ab8e817a2df52bb05c97eea4c668217eecf6b3e
fergone03 <114108805+fergone03@users.noreply.github.com>
2024-10-24 00:11:01 +0200

    animaciones
    +animaciones tumba

--- front/js/index.js
+++ front/js/index.js
@@ -4,7 +4,7 @@ function verificarNombreJose() {
 
   if (nombre === "Jose") {
     imagenDiv.style.display = "block";
-  } 
+  }
 }
 function verificarNombreJavi() {
   const nombre = document.getElementById("checkJavi").value;
@@ -12,7 +12,7 @@ function verificarNombreJavi() {
 
   if (nombre === "Javi") {
     imagenDiv.style.display = "block";
-  } 
+  }
 }
 function verificarNombreJuli() {
   const nombre = document.getElementById("checkJuli").value;
@@ -21,38 +21,54 @@ function verificarNombreJuli() {
   if (nombre === "Juli") {
     imagenDiv.style.display = "block";
     var sonido = document.getElementById("sonidoBloodborne");
-    sonido.currentTime = 0; 
-    sonido.play(); 
-  } 
-}
- function navidadEnHalloween() {
-   document.body.style.backgroundImage ="url('./assets/img/fondo_navidad.webp')";
-   document.body.style.backgroundSize ="cover";
-   document.body.style.backgroundPosition ="center";
-   document.body.style.backgroundRepeat ="no-repeat";
-   document.body.style.backgroundAttachment ="fixed";
-
-    setTimeout(() => {
-        document.body.style.backgroundImage = "url('./assets/img/fondo.webp')"; 
-        document.body.style.backgroundSize ="cover";
-        document.body.style.backgroundPosition ="center";
-        document.body.style.backgroundRepeat ="no-repeat";
-        document.body.style.backgroundAttachment ="fixed";
-    }, 3000); 
-
- }
-  function sonidoCaballo() {
-        var sonido = document.getElementById("sonidoCaballo");
-        sonido.currentTime = 0; 
-        sonido.play(); 
-    }
-  function tumbaInicio() {
-      document.body.style.backgroundColor = "black";
-      var contenedor = document.getElementsByClassName('contenedor');
-      contenedor.style.display = "none";
-      var header = document.getElementsByTagName('header');
-      header.style.display = "none";
-      var tumba = document.getElementsByClassName('tumba');
-      tumba.style.display = "block";
+    sonido.currentTime = 0;
+    sonido.play();
   }
-  
+}
+function navidadEnHalloween() {
+  document.body.style.backgroundImage =
+    "url('./assets/img/fondo_navidad.webp')";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundAttachment = "fixed";
+
+  setTimeout(() => {
+    document.body.style.backgroundImage = "url('./assets/img/fondo.webp')";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
+  }, 3000);
+}
+
+function sonidoCaballo() {
+  var sonido = document.getElementById("sonidoCaballo");
+  sonido.currentTime = 0;
+  sonido.play();
+}
+function tumbaInicio() {
+  document.body.style.backgroundImage = "url('./assets/img/fondo.webp')";
+  var tumba = document.getElementsByClassName("prePagina")[0]; 
+  tumba.style.display = "none";
+  var contenedor = document.getElementsByClassName("contenedor")[0]; 
+  contenedor.style.display = "block";
+  var header = document.getElementsByTagName("header")[0]; 
+  header.style.display = "block";
+}
+function animarManoZombie() {
+  const manoZombie = document.getElementById("manoZombie");
+  manoZombie.style.display = "block"; // Muestra la mano de zombie
+
+  // Inicia la animación
+  manoZombie.classList.add("animar");
+
+  // Espera a que la animación termine
+  manoZombie.addEventListener('animationend', function() {
+    // Oculta la mano de zombie después de la animación
+    manoZombie.style.display = "none";
+    
+    // Llama a la función tumbaInicio
+    tumbaInicio();
+  }, { once: true }); // Se asegura de que se ejecute solo una vez
+}
